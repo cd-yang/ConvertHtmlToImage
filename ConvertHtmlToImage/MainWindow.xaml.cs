b91bf76b49fc0f1f028a20f8f6f936e4bb537a5b
cd yang <1012892053@qq.com>
2021-03-09 09:49:46 +0800

    render wpf control to image

--- ConvertHtmlToImage/MainWindow.xaml.cs
+++ ConvertHtmlToImage/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using TheArtOfDev.HtmlRenderer.Core;
@@ -16,6 +20,10 @@ namespace ConvertHtmlToImage
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
 
             var style = @"
 table
@@ -96,8 +104,8 @@ table td
 
             var cssData = HtmlRender.ParseStyleSheet(style);
 
-            var img = HtmlRender.RenderToImage(_html, new Size(200, 200), new Size(999999, 999999), cssData: cssData, backgroundColor: Color.FromRgb(122,122,122));
-            
+            var img = HtmlRender.RenderToImage(_html, new Size(200, 200), new Size(999999, 999999), cssData: cssData, backgroundColor: Color.FromRgb(122, 122, 122));
+
             using (var fileStream = new FileStream(filePath, FileMode.Create))
             {
                 var encoder = new PngBitmapEncoder();
@@ -106,5 +114,54 @@ table td
             }
 
         }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+                try
+                {
+                    var thread = new Thread(() =>
+                    {
+                        var control = new Grid();
+                        var random = new Random();
+                        byte a = (byte)random.Next(64, 256);
+                        byte r = (byte)random.Next(64, 256);
+                        byte g = (byte)random.Next(64, 256);
+                        byte b = (byte)random.Next(64, 256);
+                        control.Background = new SolidColorBrush(Color.FromArgb(a, r, g, b));
+
+                        var textBlock = new TextBlock();
+                        textBlock.FontSize = 30;
+                        textBlock.Text = $"A:{a} R:{r} G:{g} B:{b}";
+                        textBlock.VerticalAlignment = VerticalAlignment.Center;
+                        textBlock.HorizontalAlignment = HorizontalAlignment.Center;
+                        control.Children.Add(textBlock);
+
+                        // will be drawn to the canvas size passing the outermost layer controls
+                        Rect renderRect = new Rect(0, 0, 500, 500);
+                        control.Arrange(renderRect); // must control property after setting up the draw to call before RenderTargetBitmap
+
+                        // Render control to RenderTargetBitmap
+                        var bitmapRender = new RenderTargetBitmap((int)renderRect.Width, (int)renderRect.Height, 96d, 96d, PixelFormats.Pbgra32);
+                        bitmapRender.Render(control);
+
+                        // RenderTargetBitmap saved to the Pictures
+                        var bmpEncoder = new PngBitmapEncoder();
+                        bmpEncoder.Frames.Add(BitmapFrame.Create(bitmapRender));
+                        using (Stream fs = File.Create(@"C:\output.png"))
+                        {
+                            bmpEncoder.Save(fs);
+                        }
+                    });
+                    thread.SetApartmentState(ApartmentState.STA); // controls must be constructed in the STA thread
+                    thread.IsBackground = true;
+                    thread.Start();
+                    thread.Join();
+                }
+                catch (Exception ex)
+                {
+                    Trace.TraceError($"{ex}");
+                }
+        }
+
     }
 }
