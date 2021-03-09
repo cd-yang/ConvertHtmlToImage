0f86433b25fc27e5cd921b2e5bd4b10a5bc79ec7
cd yang <1012892053@qq.com>
2021-03-09 11:28:02 +0800

    render html string to image with CefSharp

---
+++ ConvertHtmlToImage/CefsharpUtility.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using CefSharp;
+using CefSharp.OffScreen;
+
+namespace ConvertHtmlToImage
+{
+    class CefsharpUtility
+    {
+        private static ChromiumWebBrowser browser;
+
+        public static void GenerateImage(string htmlContent)
+        {
+#if ANYCPU
+            //Only required for PlatformTarget of AnyCPU
+            AppDomain.CurrentDomain.AssemblyResolve += Resolver;
+#endif
+
+            const string testUrl = "https://www.baidu.com/";
+
+            Console.WriteLine("This example application will load {0}, take a screenshot, and save it to your desktop.", testUrl);
+            Console.WriteLine("You may see Chromium debugging output, please wait...");
+            Console.WriteLine();
+
+            var settings = new CefSettings()
+            {
+                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
+            };
+
+            var dependencyCheck = true;
+
+#if NETCOREAPP || ANYCPU
+            //For .Net Core/.Net 5 this should be fixed as part of https://github.com/cefsharp/CefSharp/issues/3388
+            dependencyCheck = false;
+#endif
+
+            //Perform dependency check to make sure all relevant resources are in our output directory.
+            Cef.Initialize(settings, performDependencyCheck: dependencyCheck, browserProcessHandler: null);
+
+            // Create the offscreen Chromium browser.
+            //browser = new ChromiumWebBrowser(testUrl);
+            browser = new ChromiumWebBrowser(new CefSharp.Web.HtmlString(htmlContent));
+
+            // An event that is fired when the first page is finished loading.
+            // This returns to us from another thread.
+            browser.LoadingStateChanged += BrowserLoadingStateChanged;
+
+            // We have to wait for something, otherwise the process will exit too soon.
+            //Console.ReadKey();
+
+            // Clean up Chromium objects.  You need to call this in your application otherwise
+            // you will get a crash when closing.
+            //Cef.Shutdown();
+
+        }
+
+        private static void BrowserLoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            // Check to see if loading is complete - this event is called twice, one when loading starts
+            // second time when it's finished
+            // (rather than an iframe within the main frame).
+            if (!e.IsLoading)
+            {
+                // Remove the load event handler, because we only want one snapshot of the initial page.
+                browser.LoadingStateChanged -= BrowserLoadingStateChanged;
+
+                var scriptTask = browser.EvaluateScriptAsync("document.querySelector('[name=q]').value = 'CefSharp Was Here!'");
+
+                scriptTask.ContinueWith(t =>
+                {
+                    //Give the browser a little time to render
+                    Thread.Sleep(500);
+                    // Wait for the screenshot to be taken.
+                    var task = browser.ScreenshotAsync();
+                    task.ContinueWith(x =>
+                    {
+                        // Make a file to save it to (e.g. C:\Users\jan\Desktop\CefSharp screenshot.png)
+                        var screenshotPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "CefSharp screenshot.png");
+
+                        Console.WriteLine();
+                        Console.WriteLine("Screenshot ready. Saving to {0}", screenshotPath);
+
+                        // Save the Bitmap to the path.
+                        // The image type is auto-detected via the ".png" extension.
+                        task.Result.Save(screenshotPath);
+
+                        // We no longer need the Bitmap.
+                        // Dispose it to avoid keeping the memory alive.  Especially important in 32-bit applications.
+                        task.Result.Dispose();
+
+                        Console.WriteLine("Screenshot saved.  Launching your default image viewer...");
+
+                    }, TaskScheduler.Default);
+                });
+            }
+        }
+
+        // Will attempt to load missing assembly from either x86 or x64 subdir
+        //when PlatformTarget is AnyCPU
+#if ANYCPU
+        private static System.Reflection.Assembly Resolver(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.StartsWith("CefSharp.Core.Runtime"))
+            {
+                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
+                string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                                                       Environment.Is64BitProcess ? "x64" : "x86",
+                                                       assemblyName);
+
+                return File.Exists(archSpecificPath)
+                           ? System.Reflection.Assembly.LoadFile(archSpecificPath)
+                           : null;
+            }
+
+            return null;
+        }
+#endif
+    }
+}
--- ConvertHtmlToImage/MainWindow.xaml.cs
+++ ConvertHtmlToImage/MainWindow.xaml.cs
@@ -25,84 +25,9 @@ namespace ConvertHtmlToImage
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
 
-            var style = @"
-table
-{
-  min-width: 100%;
-}
-
-table td
-{
-  border: 1px solid silver;
-  position: relative;
-}
-
-.crossed
-{
-   background-image: linear-gradient(to bottom left,  transparent calc(50% - 1px), red, transparent calc(50% + 1px)); 
-}
-
-.colHeader
-{
-	text-align: right;
-}
-";
-
-            var _html = @"
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-
-<table>
-  <tr>
-    <td class=""crossed"">
-        <div class=""colHeader"">Company</div>
-		<div>Company</div>
-	</td>
-    <td>Contact</td>
-    <td>Country</td>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-</table>
-
-</body>
-</html>
-";
             var filePath = @"C:\Test.png";
 
-            var cssData = HtmlRender.ParseStyleSheet(style);
+            var cssData = HtmlRender.ParseStyleSheet(_style);
 
             var img = HtmlRender.RenderToImage(_html, new Size(200, 200), new Size(999999, 999999), cssData: cssData, backgroundColor: Color.FromRgb(122, 122, 122));
 
@@ -162,6 +87,108 @@ table td
                     Trace.TraceError($"{ex}");
                 }
         }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            CefsharpUtility.GenerateImage(_html);
+        }
+
+        const string _html = @"
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+<style>table
+{
+  min-width: 100%;
+}
+
+table td
+{
+  border: 1px solid silver;
+  position: relative;
+}
+
+.crossed
+{
+   background-image: linear-gradient(to bottom left,  transparent calc(50% - 1px), red, transparent calc(50% + 1px)); 
+}
+
+.colHeader
+{
+	text-align: right;
+}
+</style>
+<table>
+  <tr>
+    <td class=""crossed"">
+        <div class=""colHeader"">Company</div>
+		<div>Company</div>
+	</td>
+    <td>Contact</td>
+    <td>Country</td>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
+
+</body>
+</html>
+";
+
+        const string _style = @"
+table
+{
+  min-width: 100%;
+}
+
+table td
+{
+  border: 1px solid silver;
+  position: relative;
+}
+
+.crossed
+{
+   background-image: linear-gradient(to bottom left,  transparent calc(50% - 1px), red, transparent calc(50% + 1px)); 
+}
+
+.colHeader
+{
+	text-align: right;
+}
+";
+
 
     }
 }
